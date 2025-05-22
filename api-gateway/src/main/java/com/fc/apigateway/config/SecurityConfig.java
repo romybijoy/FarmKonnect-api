09ff20a65e6eeebaa65b5600ebd203dfad2b1b8b
romy <romyjimmy17@gmail.com>
2025-05-23 05:19:28 +0530

    api-gateway added

---
+++ api-gateway/src/main/java/com/fc/apigateway/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.fc.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange(exchange -> exchange.anyExchange().permitAll())
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .cors(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+}
