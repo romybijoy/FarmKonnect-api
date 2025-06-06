f233042f29c14c5a553e144a5b113baa55aa1733
romy <romyjimmy17@gmail.com>
2025-06-06 12:35:03 +0530

    model mapper added - post

--- post-service/src/main/java/com/fc/postservice/PostServiceApplication.java
+++ post-service/src/main/java/com/fc/postservice/PostServiceApplication.java
@@ -1,5 +1,6 @@
 package com.fc.postservice;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +11,11 @@ import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandl
 
 @SpringBootApplication
 public class PostServiceApplication {
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PostServiceApplication.class, args);
