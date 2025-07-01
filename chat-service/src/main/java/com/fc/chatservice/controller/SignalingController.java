35dc7a490aaba69e246ed559fa8d00ea83c5e9ba
romy <romyjimmy17@gmail.com>
2025-07-01 07:00:27 +0530

    Video call feature added

--- chat-service/src/main/java/com/fc/chatservice/controller/SignalingController.java
+++ chat-service/src/main/java/com/fc/chatservice/controller/SignalingController.java
@@ -2,10 +2,10 @@ package com.fc.chatservice.controller;
 
 import com.fc.chatservice.dto.SignalMessage;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
-import java.security.Principal;
 
 @Controller
 public class SignalingController {
@@ -16,9 +16,18 @@ public class SignalingController {
         this.messagingTemplate = messagingTemplate;
     }
 
-    @MessageMapping("/signal")
-    public void handleSignal(SignalMessage signal, Principal principal) {
-        String toUserId = signal.getTo();
-        messagingTemplate.convertAndSendToUser(toUserId, "/topic/signal", signal);
+    // Video/Audio call signaling
+    @MessageMapping("/call/signal")
+    public void handleSignal(@Payload SignalMessage message) {
+        String targetUserId = switch (message.getType()) {
+            case "offer" -> message.getReceiverId();
+            case "answer" -> message.getCallerId();
+            case "ice" -> message.getTargetId();
+            default -> "";
+        };
+
+        messagingTemplate.convertAndSend("/topic/call/" + targetUserId, message);
     }
+
+
 }
--- chat-service/src/main/java/com/fc/chatservice/dto/SignalMessage.java
+++ chat-service/src/main/java/com/fc/chatservice/dto/SignalMessage.java
@@ -3,14 +3,25 @@ package com.fc.chatservice.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+
 @Setter
 @Getter
 public class SignalMessage {
 
-    private String type; // offer / answer / candidate
-    private String from;
-    private String to;
-    private Object sdp;        // optional
-    private Object candidate;
+
+        private String type; // "offer", "answer", "ice"
+        private String callerId;
+        private String receiverId;
+
+        // For SDP (offer/answer)
+        private String sdp;
+
+        private String targetId;
+        // For ICE candidate
+        private String candidate;
+        private String sdpMid;
+        private Integer sdpMLineIndex;
+
+
 
 }
